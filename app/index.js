44289ebab485778a053f8f725d4c8d6865d1b6a2
aktechuniverse <136071965+aktechuniverse@users.noreply.github.com>
2025-05-30 16:39:44 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on custom AMI! by using packer workshop by veera');
+  res.send('Hello from bihar and full stack java python and dotnet and ada and angular and react view.js and multicloud and devsecops!......');
+  
 });
-####veera
+####Abhishek Singh
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
